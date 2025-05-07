f6384aba008aee63b9bab939ece438c6967e29b4
GlebKurt <kurtgleb007@gmail.com>
2025-05-07 17:17:21 +0200

    Applied new commands to the file

--- commands.py
+++ commands.py
@@ -14,7 +14,8 @@ BOT_COMMANDS = [
    BotCommand(command="create_film", description="Додати новий фільм"),
    BotCommand(command="search_movie", description="Пошук фільма за назвою"),
    BotCommand(command="filter_movies", description="Пошук фільма за жанром"),
-   BotCommand(command="delete_movie", description="Пошук фільма за назвою"),
-   BotCommand(command="edit_movie", description="Пошук фільма за назвою"),
-   BotCommand(command="rate_movie", description="Пошук фільма за назвою"),
+   BotCommand(command="delete_movie", description="Видалити фільм"),
+   BotCommand(command="edit_movie", description="Редагувати фільм"),
+   BotCommand(command="rate_movie", description="Оцінити фільм"),
+   BotCommand(command="recommend_movie", description="Рекомендації")
 ]
